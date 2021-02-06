6b71f7bb1b47c3077709c73220afc94f6371b8cf
Wannes <wanneske_v@hotmail.com>
2021-02-06 15:03:31 +0100

    Bit more design

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container">
-      <div id="app">
-         <h1 id="quote-setup" class="fade">{{setup}}</h1>
-         <h1 id="quote-punchline" class="fade">{{punchline}}</h1>
-         <button id="btn" class="fade" @click="tellJoke">{{buttonText}}</button>
-      </div>
+  <div id="app">
+    <div class="container">
+      <span id="setup">{{setup}}</span>
+      <span id="punchline">&nbsp;{{punchline}}</span>
+      <span id="btn" @click="tellJoke">{{buttonText}}</span>
+    </div>
   </div>
 </template>
 
@@ -39,8 +39,8 @@
            this.punchline = "";
         //});
 
-         axios.get("https://official-joke-api.appspot.com/jokes/random")
-         .then((response => (
+        axios.get("https://official-joke-api.appspot.com/jokes/random")
+        .then((response => (
             this.setup = response.data.setup,
             this.punchline = response.data.punchline,
             this.quoteUtterance = new SpeechSynthesisUtterance(this.setup),
@@ -51,7 +51,6 @@
             //quoteUtterance.pitch = personUtterance.pitch = 1.1,
             this.SYNTHESIS.speak(this.quoteUtterance),
             this.SYNTHESIS.speak(this.personUtterance),
-            this.buttonText = "Tell me another joke",
             this.quoteUtterance.onstart = function(){
               //document.getElementById('quote-setup').fadeIn(750);
             },
@@ -61,20 +60,64 @@
             this.personUtterance.onend = function(){
               //document.getElementById('btn').fadeIn(1000);
             }
-           )
-         ));
-       }
-     }
+          )
+        ));
+      }
+    }
   }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@500;900&display=swap');
+
+  body{
+    background: black;
+  }
+
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Kanit, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: white;
     margin-top: 60px;
+
+    text-transform: uppercase;
+    font-weight: 900;
+    font-size: 2em;
+  }
+
+  .container{
+    padding: 30px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: fit-content;
+    transform: translate(-50%, -50%);
+    max-width: 70vw;
+    letter-spacing: 5px;
+  }
+
+  #setup{
+    -webkit-text-stroke: 2px white;
+    text-stroke: 2px white;
+    -webkit-text-fill-color: transparent;
+    text-fill-color: transparent;
+    color: transparent;
+  }
+  #punchline{
+    color: white;
+  }
+
+  #btn{
+    background: white;
+    padding: 8px 15px;
+    color: black;
+    font-size: 15px;
+    display: block;
+    width: fit-content;
+    margin: 0 auto;
+    font-weight: 500;
+    margin-top: 50px;
   }
 </style>
